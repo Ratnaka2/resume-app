c1e1d6979b4d01dba7283a1da9258e8974e99b3f
ratnakar dashrath yadav <117161937+ratn7921@users.noreply.github.com>
2024-09-26 21:22:41 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,18 +6,16 @@ function Skills() {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-6">Skills</h2>
         <ul className="flex flex-wrap gap-4">
-<img src="src\assets\languages\javascript.gif" alt="" width={30} height={15}/>
-<img src="src\assets\languages\java.gif" alt="" width={30} height={15}/>
-<img src="src\assets\languages\github.gif" alt="" width={30} height={15}/>
-<img src="src\assets\languages\OIP.jpg" alt="" width={30} height={15}/>
-<img src="src\assets\languages\postgresql-logo.png" alt="" width={30} height={15}/>
-<img src="src\assets\languages\react.gif" alt="" width={30} height={15}/>
-<img src="src\assets\languages\st,small,507x507-pad,600x600,f8f8f8.u2.jpg" alt="" width={30} height={15}/>
-<img src="src\assets\languages\mo429m311-mongodb-logo-mongodb-logo-.png" alt="" width={30} height={15}/>
-<img src="src\assets\languages\git.gif" alt="" width={60} height={15}/>
-<img src="src\assets\languages\intellij-idea-icon-1024x1014-qi27z6hu.png" alt="" width={30} height={15}/>
-<img src="src\assets\languages\graphql-logo-freelogovectors.net_.png" alt="" width={30} height={15}/>
-       
+          {/* Replace local image paths with hosted URLs */}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="JavaScript" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg" alt="Java" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg" alt="PostgreSQL" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg" alt="MongoDB" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg" alt="Git" width={60} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/IntelliJ_IDEA_Icon.svg" alt="IntelliJ IDEA" width={30} height={15} />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg" alt="GraphQL" width={30} height={15} />
         </ul>
       </div>
     </section>
